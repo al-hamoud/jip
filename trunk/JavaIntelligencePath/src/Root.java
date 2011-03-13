7f28c6308f74876c3e27ad0a15132b9c0de3cd3c
admin@al-hamoud.com <admin@al-hamoud.com@4da48cca-89d0-01e4-e691-00f233014401>
2011-03-13 17:23:29 +0000

    updated

--- trunk/JavaIntelligencePath/src/Root.java
+++ trunk/JavaIntelligencePath/src/Root.java
@@ -24,35 +24,105 @@ import java.util.Set;
 
 public abstract class Root
 {
+    /* @access private
+     * @type static & final
+     * @String
+     * @name SLASH
+     * @value "/"
+     */
     private static final String SLASH = "/";
-    
+
+    /* @access protected
+     * @type dynamic
+     * @String
+     * @name nodeName
+     * @intent to store the name of node 'Folder or file'
+     */
     protected String nodeName = "";
 
+    /* @access private
+     * @type dynamic
+     * @StringBuffer
+     * @name path
+     * @intent to store the full path of the node
+     */
     private StringBuffer path = new StringBuffer();
 
+    /* @access protected
+     * @type dynamic
+     * @Set<Root>
+     * @name childFolder
+     * @intent to store the child node into current node
+     */
     protected Set<Root> childFolder  = new HashSet<Root>();
 
+    /* @access protected
+     * @type dynamic
+     * @Root
+     * @name successor
+     * @intent to set the parent of current node
+     */
     protected Root successor = null;
 
+    /*
+     * @method
+     * @name setSuccessor
+     * @access public
+     * @param Root successor
+     * @return void
+     * @intent to set the successor 'parent'
+     */
     public void setSuccessor(Root successor){
         this.successor = successor;
-    }
+    }/* Ending of setSuccessor method **/
 
+    /*
+     * @method
+     * @name doChain
+     * @access private
+     * @param Root next
+     * @return void
+     * @intent to get every parent in the tree
+         * untill get into the root of the tree
+         * to build the full path of the node.
+     */
     private void doChain(Root next){
-        
+
+        /* if next is null that means it's in the root of tree **/
         if(next != null){
+           
+            /*@access internal
+            * @type dynamic
+            * @String
+            * @name addPath
+            * @intent to start building the full path by giving
+             * the the current node
+            */
             String addPath = next.getNodeName() + Root.SLASH;
+
+            /* call insert method inside StringBuffer path */
             path.insert(0, addPath);
+            /* call doChain inside current node iteration by iteration*/
             this.doChain(next.getSuccessor());
         }// endinf of if stmt.
         
-    }// end of doChain 
+    }// end of doChain
+
+     /*
+     * @method
+     * @name getFullPath
+     * @access public
+     * @param void
+     * @return String
+     * @intent to retrieve the full path of the given node
+     */
     public String getFullPath(){
-        String mpath = this.getNodeName();
+        String currentPath = this.getNodeName();
         this.doChain(this.successor);
         
-        return (path.append(mpath)).toString();
-    }
+        return (path.append(currentPath)).toString();
+    }// Ending of getFullPath method
+    
     public abstract String getNodeName();
     public abstract Root getSuccessor();
     public abstract boolean IsFolder();
